46d28806e007fd23b5186c82689098115cccfdda
eduardovlemes <91350022+eduardovlemes@users.noreply.github.com>
2021-09-27 16:05:26 -0300

    Create index.py

---
+++ python/ex007/index.py
@@ -0,0 +1,19 @@
+# Aula 7 - Outras comparações - aqui a exclamação + o sinal de igual demonstram que é diferente (True ou False)
+
+print(10 == 20/2)
+
+print(10 != 10)
+
+print(10 != 15)
+
+# sinal de maior ou menor
+
+print (100 > 50)
+
+print (100 < 50)
+
+# maior que ou igual - menor que ou igual
+
+print (15 >= 10 + 10)
+
+print (15 <= 10 + 5)
