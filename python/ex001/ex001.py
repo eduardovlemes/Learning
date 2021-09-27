bb061043429814a06d277cb7b9d2acccd1f012c3
eduardovlemes <91350022+eduardovlemes@users.noreply.github.com>
2021-09-27 15:48:14 -0300

    Create ex001.py

---
+++ python/ex001/ex001.py
@@ -0,0 +1,21 @@
+#1  PYTHON - ELEMENTOS
+
+#Int
+x1 = 5
+print (type (x1))
+
+#Float
+x2 = 5.1
+print(type (x2))
+
+#Bool
+x3 = True
+print(type(x3))
+
+#Str
+x4 = "Eduardo"
+print(type(x4))
+print(x4)
+
+#para printar tipos diferentes tem que converter
+print (str(x2)+" "+ x4)
