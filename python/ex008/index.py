7d517cb52c165fe3e3022ba3b0c29b1ed43e439e
eduardovlemes <91350022+eduardovlemes@users.noreply.github.com>
2021-09-27 16:06:26 -0300

    Create index.py

---
+++ python/ex008/index.py
@@ -0,0 +1,32 @@
+# Booleans como operadores - NOT - AND - OR
+
+# NOT leva ao estado oposto
+
+print(not True)
+print(not False)
+
+# AND checa se dois argumentos são verdadeiros
+
+print(True and True)
+print(True and False)
+print(False and False)
+
+# OR procura se um dos argumentos é verdadeiro
+
+print(False or False)
+print(True or True)
+print(True or False)
+#Exemplo
+print (3>10 and 10<=20)
+
+# Lógica dos operadores NOT - AND - OR
+print (True and not True)
+print (False or not True and True)
+print (True and not True or True)
+
+# Operadores de identidade IS, IS NOT
+
+print (5 is 6)
+print (5 == 6)
+print (5 is not 6)
+print (5 != 6)
