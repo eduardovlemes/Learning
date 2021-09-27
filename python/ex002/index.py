4b3b990758910bb0c23e50361c052a3b89e76593
eduardovlemes <91350022+eduardovlemes@users.noreply.github.com>
2021-09-27 15:59:50 -0300

    Create index.py

---
+++ python/ex002/index.py
@@ -0,0 +1,19 @@
+#2  PYTHON - Operações
+
+# soma
+print (1 + 1)
+
+# subtração
+print (1 - 1)
+
+# divisão
+print (2 / 1)
+
+# multiplicação
+print(1 * 3)
+
+# potência
+print (5 ** 2)
+
+#resto da divisão
+print (16 %3 )
