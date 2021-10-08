b7f02af7c44534925a8dacf531434addf2e74458
Eduardo V. Lemes <91350022+eduardovlemes@users.noreply.github.com>
2021-10-07 21:17:38 -0300

    Create index.js

---
+++ javascript/ex006/index.js
@@ -0,0 +1,7 @@
+//Ex. condição simples
+var vel = 55
+console.log(`A velocidade do seu carro é ${vel}km/h.`)
+if (vel>60) {
+    console.log('Você ultrapassou a velocidade permitida, MULTADO!')
+}
+console.log('Dirija sempre usando sinto de segurança!')
